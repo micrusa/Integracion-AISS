51b5c67a44cb11707c843da4de076acf8cf34bc2
Miguel Cruces <micrusa@hotmail.es>
2025-04-01 16:23:27 +0200

    Lab5: Consumo de servicios RESTful (III)
    Faltan ejercicios 4 y 5: Para el 4 habría q añadir try/catch y para el 5 usar copiar el UserService para la otra pág usando restTemplate.exchange con headers (ver CommitService.getForAuthenticated para ejemplo)

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -3,9 +3,13 @@ package com.example.demo.services;
 import com.example.demo.model.user.ListUsers;
 import com.example.demo.model.user.SingleUser;
 import com.example.demo.model.user.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @Service
@@ -24,5 +28,20 @@ public class UserService {
     public User findUser(int id) {
         SingleUser obj = restTemplate.getForObject("https://reqres.in/api/users/" + id, SingleUser.class);
         return obj == null ? null : obj.getData();
+    }
+
+    public User createUser(User user) {
+        User newUser = restTemplate.postForObject("https://reqres.in/api/users", user, User.class);
+        return newUser;
+    }
+
+    public User updateUser(int id, User user) throws URISyntaxException {
+        HttpEntity<User> req = new HttpEntity<>(user);
+        User updatedUser = restTemplate.exchange(new URI("https://reqres.in/api/users/" + id), HttpMethod.PUT, req, User.class).getBody();
+        return user;
+    }
+
+    public void deleteUser(int id) {
+        restTemplate.delete("https://reqres.in/api/users/" + id);
     }
 }
--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -26,7 +28,7 @@ public class UserServiceTest {
         List<User> users = userService.findAllUsers(1);
         assertNotNull(users, "Users list is null");
         assertFalse(users.isEmpty(), "Users list is empty");
-        //System.out.println("Users: " + users);
+        System.out.println("Users: " + users);
     }
 
     @Test
@@ -35,7 +37,50 @@ public class UserServiceTest {
         for(int id : new int[]{1,2,3}) {
             User user = userService.findUser(id);
             assertNotNull(user, "User is null");
-            //System.out.println("User " + id + ": " + user);
+            System.out.println("User " + id + ": " + user);
+        }
+    }
+
+    @Test
+    @DisplayName("Create User")
+    public void createUser() {
+        User user = new User();
+        user.setFirstName("Juan");
+        user.setEmail("juan@gmail.com");
+        User newUser = userService.createUser(user);
+        assertNotNull(newUser, "New user is null");
+        assertNotNull(newUser.getId(), "New user ID is null");
+        System.out.println("Created User: " + newUser);
+    }
+
+    @Test
+    @DisplayName("Update User")
+    public void updateUser() throws URISyntaxException {
+        User user = new User();
+        user.setFirstName("Juan");
+        user.setEmail("juan@gmail.com");
+        User newUser = userService.createUser(user);
+
+        user.setEmail("juan1@gmail.com");
+        User updatedUser = userService.updateUser(newUser.getId(), user);
+
+        assertNotNull(updatedUser, "Updated user is null");
+        assertEquals(updatedUser.getEmail(), user.getEmail(), "User email not updated");
+    }
+
+    @Test
+    @DisplayName("Delete User")
+    public void deleteUser() {
+        User user = new User();
+        user.setFirstName("Juan");
+        user.setEmail("juan@gmail.com");
+        User newUser = userService.createUser(user);
+        userService.deleteUser(newUser.getId());
+        try {
+            User deletedUser = userService.findUser(newUser.getId());
+            assertNull(deletedUser, "Deleted user is not null");
+        } catch(HttpClientErrorException.NotFound ignore) {
+            // Pass
         }
     }
 }
